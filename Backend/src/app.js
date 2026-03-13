02a549a9f0efa0c8117808325e1358b74c19efec
Rohit Yadav <rohit@gmail.com>
2026-03-14 00:42:04 +0530

    update backend API url

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -11,7 +11,7 @@ app.use(cookieParser())
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://resume-generate-git-main-rohitkumar-yadavs-projects.vercel.app"
+    "https://resume-generate-two.vercel.app"
   ],
   credentials: true
 }))
