d51db5c33efe73f7c33cad97c0ce121e769f79a7
Rohit Yadav <rohit@gmail.com>
2026-03-14 00:58:21 +0530

    update backend API url

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 
 const api = axios.create({
-    baseURL: "https://resume-generate-two.vercel.app",
+    baseURL: "https://resume-generate-lztt.onrender.com",
     withCredentials: true
 })
 
