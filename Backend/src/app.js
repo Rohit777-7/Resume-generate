5af43d8f84adffd30d6d084c01765ff69eb101bb
Rohit Yadav <rohit@gmail.com>
2026-03-13 23:01:39 +0530

    update backend API url

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,11 +6,15 @@ const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true
-}))
 
+
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://resume-generate-gxgaed2n1-rohitkumar-yadavs-projects.vercel.app"
+  ],
+  credentials: true
+}))
 /* require all the routes here */
 const authRouter = require("./routes/auth.routes")
 const interviewRouter = require("./routes/interview.routes")
