77bbc74a278e2533eff10021cfc9b8a31347326a
Rohit Yadav <rohit@gmail.com>
2026-03-14 00:26:52 +0530

    update backend API url

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -6,11 +6,11 @@ const api = axios.create({
     withCredentials: true
 })
 
-export async function register({ username, email, password }) {
+export async function register({ name, email, password }) {
 
     try {
         const response = await api.post('/api/auth/register', {
-            username, email, password
+            name, email, password
         })
 
         return response.data
