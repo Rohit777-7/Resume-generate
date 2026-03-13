5d3a86198984dd8d3c932c4f1bdb34e426be3f57
Rohit Yadav <rohit@gmail.com>
2026-03-13 22:14:49 +0530

    fix protected component path

--- Frontend/src/app.routes.jsx
+++ Frontend/src/app.routes.jsx
@@ -2,7 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 
 import Login from "./features/auth/pages/Login.jsx";
 import Register from "./features/auth/pages/Register.jsx";
-import Protected from "./features/auth/components/Protected.jsx";
+import Protected from "./features/auth/components/protected.jsx";
 
 import Home from "./features/interview/pages/Home.jsx";
 import Interview from "./features/interview/pages/Interview.jsx";
